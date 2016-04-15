67ba7f82115f23fef3482bfb82ba98be8bcfc654
Hari Dahal <hxd1478@rit.edu>
2016-04-15 17:01:41 -0400

    Initial Overall revision

--- src/MastermindGraphicalVC.java
+++ src/MastermindGraphicalVC.java
@@ -1,3 +1,10 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
 import java.util.Observable;
 import java.util.Observer;
 
@@ -7,31 +14,53 @@ import java.util.Observer;
  * Created on 4/13/2016
  */
 
-class MastermindGraphicalVC implements Observer{
+class MastermindGraphicalVC extends Application implements Observer{
 
     private MastermindModel model;
+    private TextField displayThings;
 
-    public MastermindGraphicalVC(){
+    @Override
+    public void init(){
         this.model = new MastermindModel();
-        this.model.addObserver(this);
+        model.addObserver(this);
+        //super.init();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FlowPane pane = new FlowPane();
+
+        Button btn = new Button("Click me!");
+        //btn.setOnAction(event -> model.);
+        this.displayThings = new TextField("Displaying...");
+        displayThings.setEditable(false);
+
+        pane.getChildren().addAll(btn, displayThings);
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setTitle("Mastermind Game");
+
+        primaryStage.show();
 
     }
 
-    private void playGame(){
+
+    /*private void playGame(){
         doTheThing();
     }
 
     private void doTheThing() {
         System.out.println("Doing the thing...");
         // the main command loop kind
-    }
+    }*/
 
     @Override
     public void update(Observable o, Object arg) {
-        displayGame();
+        assert o == this.model: "Unexpected subject of observation";
+        displayThings.setText("Displaying....");
+        //displayGame();
     }
 
-    private void displayGame() {
+    /*private void displayGame() {
         displayBoard();
         displayMessage();
         displayGuessStatus();
@@ -50,14 +79,14 @@ class MastermindGraphicalVC implements Observer{
     private void displayBoard() {
         System.out.println("This is UI board!!!");
     }
-
+*/
     /**
      * The main method used to play a game.
      *
      * @param args Command line arguments -- unused
      */
     public static void main(String[] args) {
-        MastermindGraphicalVC game = new MastermindGraphicalVC();
-        game.playGame();
+        //game.playGame();
+        Application.launch(args);
     }
 }
